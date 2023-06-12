37974bab4024e118f6ca76b4560a5cbc5a6c70e7
Riccardo <riccardodarcangelo@live.de>
2023-06-12 08:45:34 +0200

    booking page added

--- src/pages/BookingPage.js
+++ src/pages/BookingPage.js
@@ -3,7 +3,29 @@
 export default function BookingPage() {
     return (
         <main>
-            Hello World
+            <section className="container booking-form">
+                <form>
+                    <label for="res-date">Choose date</label>
+                    <input type="date" id="res-date"/>
+                    <label for="res-time">Choose time</label>
+                    <select id="res-time ">
+                        <option>17:00</option>
+                        <option>18:00</option>
+                        <option>19:00</option>
+                        <option>20:00</option>
+                        <option>21:00</option>
+                        <option>22:00</option>
+                    </select>
+                    <label for="guests">Number of guests</label>
+                    <input type="number" placeholder="1" min="1" max="10" id="guests"/>
+                    <label for="occasion">Occasion</label>
+                    <select id="occasion">
+                        <option>Birthday</option>
+                        <option>Anniversary</option>
+                    </select>
+                    <input type="submit" value="Make Your reservation"/>
+                </form>
+            </section>
         </main>
     );
 }
